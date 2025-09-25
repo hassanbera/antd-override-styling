b0783d44a3c3827b36e4c185e3e10edcb34488db
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2025-09-25 18:05:48 +0300

    to be continued

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,52 @@
-import { Input, Space, Typography } from 'antd'
+import { Button, Input, Space, Typography, DatePicker } from 'antd'
 import "antd/dist/reset.css";
 import './App.css';
 import './override.scss'
 
+function CustomInput1 () {
+  return (
+    <div className='customInput1'>
+      <Input placeholder='Type Here'></Input>;
+    </div>
+  )
+}
+
+function CustomInput2 () {
+  return (
+    <div className='customInput2'>
+      <Input placeholder='Type Here'></Input>;
+    </div>
+  )
+}
+
+
 const App = () => {
   return (
+   
     <div className='App'>
-      <header>
+      <header className='App-header'>
+       <Space size={12} direction='vertical'>
         <Typography.Text className="header">
           Code With Hasso
         </Typography.Text>
         <span className='welcomeMessage'>
-          asdaasda
           <Typography.Text>
             Welcome Back!
           </Typography.Text>
         </span>
-        <Input placeholder='aquamarine'></Input>
-
+        <CustomInput1/>
+        <CustomInput2/>
+        <Button>Button</Button>
+    <DatePicker style ={{width: "100%"}}   open={true} className="datePicker" classNames={{
+    popup: {
+      root: "datePickerCell"
+    }
+  }}/>
+</Space>
       </header>
 
     </div>
+    
   )
 }
 
